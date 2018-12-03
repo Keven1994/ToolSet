c3e2576a69a476055821a6251e607294442d74bf
HOHOHO <keven.kloeckner@gmail.com>
2018-12-03 03:19:53 +0100

    added license

--- ToolSet/src/toolset.cpp
+++ ToolSet/src/toolset.cpp
@@ -1,3 +1,23 @@
+/*Copyright (c) <2018> <Keven Klöckner>
+
+Permission is hereby granted, free of charge, 
+to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
 #define _CRTDBG_MAP_ALLOC  
 //#define DEBUG
 #include "../SmartVector.hpp"
@@ -72,7 +92,7 @@ namespace gtest{
 int main() {
 	static_assert(gtest::checkPointerPointer<A**>::value, "fail");
 	constexpr auto measuresize = 5000;
-	using tvec = vector<A**,setting2> ;
+	using tvec = vector<A*,setting2> ;
 	tvec b;
 	{
 		int n1 = 0, n2 = 1;
